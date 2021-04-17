01abf261e6b0e7000566549f1b866e44f99786d7
Daniel Saputro <udanieru@gmail.com>
2021-04-17 13:39:36 +0700

    commit base non model migrate

---
+++ resources/views/home.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-md-3 mb-3">
+            <div>
+                <ul class="list-group">
+                    <li class="list-group-item active" aria-current="true">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td><img src="resources\img\Screenshot (1038).png" class="rounded-circle" style="width: 50px"></td>
+                                    <td class="ps-3 align-middle"><a href="/profile" class="text-light fs-6" style="text-decoration: none">{{ Auth::user()->name }}</a></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </li>
+                    <li class="list-group-item">Mengikuti</li>
+                    <li class="list-group-item">Pengikut</li>
+                    <li class="list-group-item">Profile</li>
+                </ul>
+            </div>
+        </div>
+        <!-- create post -->
+        <div class="col-md">
+            <div>
+                <form action="/create" class="row gy-2 gx-3 align-items-center" method="POST">
+                    @csrf
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td colspan="2"><textarea class="form-control" name="post" rows="3" placeholder="Apa yang Anda pikirkan?" style="resize: none;"></textarea></td>
+                            </tr>
+                            <tr>
+                                <td><input id="profile_image" type="file" class="form-control" name="post_image"></td>
+                                <td class="text-end"><button type="submit" class="btn btn-primary">Post</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+            <!-- post -->
+            <div class="mt-3">
+                <div class="card">
+                    <div class="card-header bg-primary">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td><img src="resources\img\Screenshot (1038).png" class="rounded-circle" style="width: 30px"></td>
+                                    <td class="ps-2 align-middle"><a href="/profile" class="text-light fs-6" style="text-decoration: none">{{ Auth::user()->name }}</a></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="card-body">
+                      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, amet culpa sed eligendi itaque harum pariatur quam magnam id rerum omnis libero officia maiores odio blanditiis in adipisci quisquam placeat?</p>
+                      <img src="resources\img\social-media-users-post-banner.png" style="height: 300px" class="mx-auto d-block">
+                      <!-- list comment -->
+                        <div class="card mt-3">
+                            <div class="card-header">
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td><img src="resources\img\Screenshot (1038).png" class="rounded-circle" style="width: 30px"></td>
+                                            <td class="ps-2 align-middle"><a href="/profile" class="text-dark" style="text-decoration: none">{{ Auth::user()->name }}</a></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam iure neque aut commodi iste, nesciunt exercitationem aperiam alias ea, soluta fuga excepturi voluptates deserunt illo. Asperiores neque in corrupti architecto!</p>
+                                <button class="btn btn-primary btn-sm"><i class="fa fa-thumbs-o-up pe-1"></i>Suka</button>
+                            </div>
+                        </div>
+                        <form action="/post/comment" method="post">
+                            @csrf
+                            <textarea class="form-control mt-1 mb-3" rows="1" placeholder="Komentar" style="resize: none;" id="comment"></textarea>
+                            <button class="btn btn-primary"><i class="fa fa-thumbs-o-up pe-1"></i>Suka</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/profile/profile.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-3">
+    <table>
+        <tbody>
+            <tr>
+                <!-- profile -->
+                <td rowspan="3"><img src="resources\img\Screenshot (1038).png" class="rounded" style="width: 200px"></td>
+                <td class="fs-2 ps-3">{{ Auth::user()->name }}</td>
+            </tr>
+            <tr>
+                <!-- bio -->
+                <td class="ps-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed veniam accusantium, numquam fugiat quod commodi saepe deserunt optio perspiciatis quos illo minima ullam, officiis rerum? Animi deleniti maiores explicabo libero.</td>
+            </tr>
+            <tr>
+                <!-- followers/ing -->
+                <td class="ps-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi voluptatem soluta ipsum dolore quasi commodi molestiae at dicta nam? Eum obcaecati necessitatibus fuga ea repellat, assumenda temporibus cupiditate sed voluptatum?</td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <!-- post -->
+                    <div class="mt-3">
+                        <div class="card">
+                            <div class="card-header  bg-primary">
+                                <span class="text-light fs-6">{{ Auth::user()->name }}</span>
+                            </div>
+                            <div class="card-body">
+                              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, amet culpa sed eligendi itaque harum pariatur quam magnam id rerum omnis libero officia maiores odio blanditiis in adipisci quisquam placeat?</p>
+                              <img src="resources\img\social-media-users-post-banner.png" style="height: 300px" class="mx-auto d-block">
+                              <!-- list comment -->
+                                <div class="card mt-3">
+                                    <p class="card-header">{{ Auth::user()->name }}</p>
+                                    <div class="card-body">
+                                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam iure neque aut commodi iste, nesciunt exercitationem aperiam alias ea, soluta fuga excepturi voluptates deserunt illo. Asperiores neque in corrupti architecto!</p>
+                                        <button class="btn btn-primary btn-sm"><i class="fa fa-thumbs-o-up pe-1"></i>Suka</button>
+                                    </div>
+                                </div>
+                              <textarea class="form-control mt-1 mb-3" rows="1" placeholder="Komentar" style="resize: none;" id="comment"></textarea>
+                              <button class="btn btn-primary"><i class="fa fa-thumbs-o-up pe-1"></i>Suka</button>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+@endsection
