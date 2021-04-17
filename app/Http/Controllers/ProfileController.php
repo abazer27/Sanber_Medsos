df5c92815462447955004ec39af2eab4699fcab8
Akbar Hazura <akbarhazura@gmail.com>
2021-04-17 07:20:12 +0700

    tes

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use app\profile;
+class ProfileController extends Controller
+{
+    public function index($id){
+        $profile = profile::find($id);
+        return view('profile.index');
+    }
+    public function edit($id){
+        $profile = profile::find($id);
+        return view('profiles.edit');
+    }
+    public function update($id, Request $request){
+
+        $request->validate([
+            'biodata' => 'required',
+        ]);
+
+        $profile = profile::where('id' , $id)->update([
+        'biodata'  => $request['biodata'],
+
+        ]);
+        return  redirect('profiles.index');
+    }
+}
+
